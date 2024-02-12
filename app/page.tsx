58365ec7b8c427a458ca8faefbe4c984de9e8de5
Apolo Freitas <apolobrener11@gmail.com>
2024-02-11 22:36:12 -0400

    fix: remove mockup data

--- app/page.tsx
+++ app/page.tsx
@@ -6,26 +6,13 @@ export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const wishes = [
-    { name: "asefd 1", uri: "sdkfjsdkf" },
-    { name: "asefd 2", uri: "sdkfjsdkf" },
-    { name: "asefd 3", uri: "sdkfjsdkf" },
-    { name: "asefd 4", uri: "sdkfjsdkf" },
-  ];
-
-  const wishesFromDb = await db.query.wishes.findMany();
+  const wishes = await db.query.wishes.findMany();
 
   return (
     <main className="flex flex-col items-center">
       <div className="flex flex-col items-center w-full max-w-screen-md">
         <ThemeToggle />
-        <pre>{JSON.stringify(wishesFromDb, null, 2)}</pre>
-        {wishes.map((wish) => (
-          <div className="bg-card border border-border">
-            <p>{wish.name}</p>
-            <Button>oiii</Button>
-          </div>
-        ))}
+        <pre>{JSON.stringify(wishes, null, 2)}</pre>
       </div>
     </main>
   );
